ecb9aec5687a7bde7e85e63ca832e578ef749b52
Ryan Prichard <ryan.prichard@gmail.com>
2014-10-15 20:27:27 -0700

    Ensure that print! with a single argument still goes through format_args!
     * For consistency, the single argument should still be considered a format
       string -- it should be a compile-time error if the print! argument
       refers to arguments.

--- src/stage1/macros.rs
+++ src/stage1/macros.rs
@@ -1,17 +1,10 @@
 #![macro_escape]
 
 macro_rules! print(
-    ($msg:expr) => (::io::print_str($msg));
-    ($fmt:expr, $($arg:tt)*) => (
-        format_args!(::io::print_args, $fmt, $($arg)*)
-    );
+    ($($arg:tt)*) => (format_args!(::io::print_args, $($arg)*));
 )
 
 macro_rules! println(
-    ($msg:expr) => (
-        print!(concat!($msg, "\r\n"))
-    );
-    ($fmt:expr, $($arg:tt)*) => (
-        print!(concat!($fmt, "\r\n"), $($arg)*)
-    );
+    ($fmt:expr) => (print!(concat!($fmt, "\r\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\r\n"), $($arg)*));
 )
