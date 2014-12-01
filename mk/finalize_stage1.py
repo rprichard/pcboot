be882008d8f07f8e441443a7779dbddfdc65e6c7
Ryan Prichard <ryan.prichard@gmail.com>
2014-12-01 01:12:31 -0800

    Fix off-by-one: allow a pre-padded stage1.bin of exactly 14332 bytes.

--- mk/finalize_stage1.py
+++ mk/finalize_stage1.py
@@ -7,7 +7,7 @@ STAGE1_SECTORS = 28
 with open("build/stage1/stage1.bin", "r") as f:
     data = f.read()
 padded_size = STAGE1_SECTORS * 512 - 4
-assert len(data) < padded_size
+assert len(data) <= padded_size
 data += chr(0) * (padded_size - len(data))
 data += struct.pack("<I", crc32c.crc32c(data))
 with open("build/stage1.bin", "w") as f:
